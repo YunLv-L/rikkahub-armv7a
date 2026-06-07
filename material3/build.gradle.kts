0250edee871fbb22f0e74acfca4db4bff8bf0de7
YunLv <luoshaoye.yyds.0812@gmail.com>
2026-06-07 16:23:24 +0800

    Add materialKolor.utilities dependency

--- material3/build.gradle.kts
+++ material3/build.gradle.kts
@@ -31,4 +31,5 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.material3)
     implementation("com.google.android.material:material:1.12.0")
+    implementation(libs.materialKolor.utilities)
 }
